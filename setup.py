15220566f73b9c24af49b24d5cdf4dbb2a64f92b
Joel Eliason <joel.eliason119@gmail.com>
2020-05-01 16:01:23 -0600

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The Chi Finesse's repo for the Trends Neuroimaging Kaggle competition',
+    author='Pariveda Chi Finesse',
+    license='MIT',
+)
